78a64bdbc8ce6a14fbc543ba95bbaa4c3fd2aa56
Andrew <andrewras93@gmail.com>
2021-05-24 14:55:07 +0200

    Added responsive front page jumbotron.

--- index.php
+++ index.php
@@ -18,6 +18,24 @@
 
     <?php include "header.php"; ?>
 
+    <div id="front-page-jumbotron" class="d-flex justify-content-center align-items-center text-center">
+
+        <div class="jumbotron container">
+
+            <h1 class="display-1">Velkommen hos L’æKARi</h1>
+
+            <p class="lead">Smag vores lækre hjemmelavet donuts eller få din helt egen
+                kage på bestilling, hos din lokale konditor i Slagelse.</p>
+
+            <div class="mt-3">
+                <a class="btn btn-light text-uppercase rounded-0 mt-1" href="bestilling.php">Bestilling</a>
+                <a class="btn text-uppercase rounded-0 mt-1" href="donuts.php">Se Udvalg</a>
+            </div>
+
+        </div>
+
+    </div>
+
     <script src="js/script.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
