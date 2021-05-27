8635fab743c6c9d175e246e43f6c1a302734b016
Andrew <andrewras93@gmail.com>
2021-05-27 22:55:16 +0200

    Added footer to every page.

---
+++ footer.php
@@ -0,0 +1,89 @@
+<footer>
+
+    <div class="container pt-5 pb-5">
+
+        <div class="row pt-lg-5 pb-lg-5">
+
+            <div class="col-md-6 col-lg-4">
+
+                <h4 class="text-uppercase text-decoration-underline">Kontakt Os</h4>
+
+                <div id="locationPin" class="d-flex mt-5">
+
+                    <div>
+                        <img src="images/location.svg" alt="Lokation logo" class="img-fluid">
+                    </div>
+
+                    <div class="ms-3">
+                        <p>L’æKARi <br>
+                        Østerbro 67, 4200 Slagelse</p>
+                    </div>
+
+                </div>
+
+                <div id="phonePin" class="d-flex align-items-center mt-5 mb-5">
+
+                    <div>
+                        <img src="images/phone.svg" alt="Telefon logo" class="img-fluid">
+                    </div>
+
+                    <div class="ms-3">
+                        <p>+45 23 60 21 22</p>
+                    </div>
+
+                </div>
+
+                <div id="emailPin" class="d-flex">
+
+                    <div>
+                        <img src="images/email.svg" alt="Email logo" class="img-fluid">
+                    </div>
+
+                    <div class="ms-3">
+                        <p>laekari@outlook.com</p>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div  id="openingHoursPin" class="col-md-6 col-lg-4 mt-5 mt-md-0">
+
+                <h4 class="text-uppercase text-decoration-underline">Åbningstider</h4>
+
+                <div class="d-flex mt-5">
+
+                    <div>
+                        <img src="images/clock.svg" alt="Lokation logo" class="img-fluid">
+                    </div>
+
+                    <div class="ms-3">
+                        <p>Mandag - Søndag<br>
+                        07.30 - 17.00</p>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div  id="orderingPin" class="mt-5 mt-lg-0 col-md-12 col-lg-4">
+
+                <h4 class="text-uppercase text-decoration-underline">Bestilling</h4>
+
+                <div class="d-flex align-items-start mt-5">
+
+                    <img src="images/order-icon.svg" alt="Lokation logo" class="img-fluid">
+
+                    <p class="ms-3">Med vores online bestillingsformular kan du nemt og hurtigt bestille dine foretrukne donuts eller få lavet en personlig kage bestilling.</p>
+
+                </div>
+
+                <a href="bestilling.php" class="rounded-0 border-0 btn btn-secondary text-uppercase mt-4">Gå Til Bestilling</a>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</footer>
